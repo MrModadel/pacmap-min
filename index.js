2c25137d2be605ed03773fc7e1d4e5161d687264
MrModadel <timanormu@gmail.com>
2023-04-08 14:27:55 +0500

    news

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ if (start) {
          if (time <= 0) {
             alert(`за ${save}-секунд ${point}-очков`)
             clearInterval(intir)
-            close()
+            document.location.href = "https://www.google.co.uz/";
          }
       }, 1000)
       let d22 = document.querySelector('.d22')
@@ -64,16 +64,16 @@ if (start) {
          d22.style.translate = `${x}px ${y}px`
          let d22_pos = d22.getBoundingClientRect()
          if (d22_pos.x >= food_pos.x - 117 && d22_pos.x <= food_pos.x + 58.5 && d22_pos.y >= food_pos.y - 117 && d22_pos.y <= food_pos.y + 58.5) {
-            let xFood = Math.round(Math.random() * winW)
+            let xFood = Math.round(Math.random() * winW-50)
             let yFood = Math.round(Math.random() * winH - 190)
-            function infinit (){
-                  yFood = Math.round(Math.random() * winH - 190)
-                  if (yFood<=0){
+            function infinit() {
+               yFood = Math.round(Math.random() * winH - 190)
+               if (yFood <= 0) {
                   infinit()
-                  }
+               }
             }
-            if (yFood<=0){
-            infinit ()
+            if (yFood <= 0) {
+               infinit()
             }
             food.style.translate = `${xFood}px ${yFood}px`
             food.style.top = '184px'
@@ -84,8 +84,8 @@ if (start) {
          }
       }
    } else {
-   close()
+      document.location.href = "https://www.google.co.uz/";
    }
 } else {
-   close()
+   document.location.href = "https://www.google.co.uz/";
 }
